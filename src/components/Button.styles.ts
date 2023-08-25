4f38265ccd039508bc5281af6595ab0189f66c15
Daniel Mota <danielgmrs@outlook.com>
2023-08-25 15:23:19 -0300

    feat: install theme

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,24 +1,21 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { ButtonVariant } from "./Button.types";
 
 interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: "purple",
-  secondary: "orange",
-  danger: "red",
-  success: "green",
-};
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-
-  ${(props) => {
-    return css`
-      background-color: ${buttonVariants[props.variant]};
-    `;
-  }}
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  background-color: ${(props) => props.theme["green-500"]};
 `;
+
+//   ${(props) => {
+//     return css`
+//       background-color: ${buttonVariants[props.variant]};
+//     `;
+//   }}
