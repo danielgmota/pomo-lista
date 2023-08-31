a6d84ecf9b0f7341253e1ea4f8204a764e07eb82
Daniel Mota <danielgmrs@outlook.com>
2023-08-31 12:10:02 -0300

    refactor: using immer produce

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,4 +1,5 @@
 import { ActionTypes } from "./actions";
+import { produce } from "immer";
 
 export interface ICycle {
   id: string;
@@ -16,24 +17,25 @@ interface ICycleState {
 
 export function cyclesReducer(state: ICycleState, action: any) {
   switch (action.type) {
+    // USANDO IMMER
     case ActionTypes.ADD_NEW_CYCLE:
-      return {
-        ...state,
-        cycles: [...state.cycles, action.payload.newCycle],
-        activeCycleId: action.payload.newCycle.id,
-      };
-    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
-      return {
-        ...state,
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.activeCycleId) {
-            return { ...cycle, interruptedDate: new Date() };
-          } else {
-            return cycle;
-          }
-        }),
-        activeCycleId: null,
-      };
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload.newCycle);
+        draft.activeCycleId = action.payload.newCycle.id;
+      });
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
+      const currentCycleIndex = state.cycles.findIndex(
+        (cycle) => cycle.id === state.activeCycleId
+      );
+
+      if (currentCycleIndex < 0) return state;
+
+      return produce(state, (draft) => {
+        draft.cycles[currentCycleIndex].startDate = new Date();
+        draft.activeCycleId = null;
+      });
+    }
+    // USANDO IMUTABILIDADE
     case ActionTypes.MARK_CURRENT_TYPE_AS_FINISHED:
       return {
         ...state,
